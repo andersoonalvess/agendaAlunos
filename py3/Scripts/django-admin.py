b23a110010efc4cabe93c32647b7cbe11185cb7f
anderson <andersoonalves@gmail.com>
2018-09-20 00:22:33 -0300

    Projeto base

---
+++ py3/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\ander\documents\django-agenda_de_eventos\py3\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
